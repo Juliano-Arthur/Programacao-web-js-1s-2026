0f09f2349452abf7db7b2eb9a967f18a496b66a9
Juliano-Arthur <zonatede@gmail.com>
2026-03-03 09:24:01 -0300

    add atividade1

---
+++ atividade1/variaveisoperadores.js
@@ -0,0 +1,14 @@
+const x = 2;
+const y = 3;
+
+const soma = x + y;
+console.log(`O valor da soma de ${x} e ${y} é ${soma}`);
+
+const sub = x - y;
+console.log(`O valor da soma de ${x} e ${y} é ${sub}`);
+
+const multi = x * y;
+console.log(`O valor da soma de ${x} e ${y} é ${multi}`);
+
+const div = x / y;
+console.log(`O valor da soma de ${x} e ${y} é ${div}`);
