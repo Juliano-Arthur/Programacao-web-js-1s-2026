f5b69d5fe4959d81b45922369e1ae552c4cb8dd5
Juliano-Arthur <zonatede@gmail.com>
2026-03-23 21:11:44 -0300

    add funcoesModulos

---
+++ jsbasico/operacoes.js
@@ -0,0 +1,18 @@
+/*
+Recebe dois números (a, b)
+Retorna a soma entre a e b
+Retorna a + b
+*/
+
+function somar(a, b) {
+    return a = b;
+}
+
+function subtrair(a, b) {
+    return a - b;
+}
+
+module.exports = {
+    somar,
+    subtrair
+}
